e28b3289d82c056f6e2baa14a8c50705ea735a1c
jluispbraga <jluspbraga@gmail.com>
2020-06-05 18:46:52 -0400

    cadrasto java

---
+++ scripts/intro.js
@@ -0,0 +1,65 @@
+//comments
+//document.writte("Hello)
+
+//variaveis, tipo de dados
+//let myvar = "her"
+//const my const = "he"
+
+//document.write(myconst + my var)
+
+//string
+//"Isso é uma string"
+//'Isso também é um string'
+//`Isso é uma string também`
+
+//number
+//const n1 = 1
+//const n2 = 12
+//documente.write(n1 + n2)
+
+//boolean- true ou false
+//const bTrue = true
+//const bFalse = false
+//document.write(bFalse)
+
+//objeto possuem propriedades e funcionalidade
+//const pessoa = {
+    //altura: "1,80m",
+    //idade: 24
+    //solteiro: true
+    //correr(){
+        //document.write("executar uma grande logica de correr")
+//    }
+//}
+
+//pessoa.correr()
+
+//Array - Vetores
+//const colecaoDeBolinhas - ["blue", "green", 1, {name: "My Name"}]
+
+//Funcoes - Funcionalidades - Atalhos - Reuso de codigo
+
+// // tegistrar funcao
+//function sayMyName(name){
+
+//documente.write(name)
+//}
+
+// //executar
+//sayMyName("Douglas")
+//sayMyName("Valeska")
+
+//condicionais
+
+//const notaFinal = 7
+
+//if( notaFinal < 5) {
+//    document.write("Reprovado")
+//} else {
+    //documente.write("Aprovado")
+//}
+
+// loop repeticoes
+//for (i = 0 ; i < 10; i++) {
+    //documente.write(`<p>${i}</p>)
+//}
